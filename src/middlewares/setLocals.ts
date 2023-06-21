aedb34ab060eaf0fdfce4e00e3c75a5112143ea7
tacia68 <tacia68@gmail.com>
2023-06-20 20:48:45 -0400

    exercicio

---
+++ src/middlewares/setLocals.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from "express";
+
+const setLocals = (req: Request, res: Response, next: NextFunction) =>{
+
+    res.locals.logado = req.cookies["logado"];
+    next();
+
+};
+
+export default setLocals;
